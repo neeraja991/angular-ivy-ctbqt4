f7e4144ed7c8dd28e671d3922ba6bc67623b33f2
neeraja991 <neerajaganesh@gmail.com>
2021-02-26 14:00:54 -0500

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component, VERSION } from "@angular/core";
+//import { employeesdata } from "../../employees.json";
+import employeesdata from "./employees.json";
+
+interface Employee {
+  Name: String;
+  Expertisefield: String;
+  CountryofBirth: String;
+  DOJ: String;
+  id: Number;
+}
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+
+  employees: Employee[] = employeesdata;
+}
+
+/*import { Component } from "@angular/core";
+import studentsData from "./students.json";
+
+interface Student {
+  id: Number;
+  name: String;
+  email: String;
+  gender: String;
+}
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  name = "Angular";
+
+  students: Student[] = studentsData;
+}*/
